a6b1ad6da8b942e0840b54fcb37b15582218d187
Insuk Lee <66675919+insukL@users.noreply.github.com>
2023-03-13 15:44:59 +0900

    init commit

---
+++ src/main/java/com/yomakase/yomakase/YomakaseApplication.java
@@ -0,0 +1,13 @@
+package com.yomakase.yomakase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomakaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YomakaseApplication.class, args);
+	}
+
+}
