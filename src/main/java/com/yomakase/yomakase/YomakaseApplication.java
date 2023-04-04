d5e4715165674ddf8ff19762a69d9fdda7133721
Insuk Lee <66675919+insukL@users.noreply.github.com>
2023-04-04 22:18:37 +0900

    feat: add base entity
    - jpa auditing 설정 추가
    - base entity 추가

--- src/main/java/com/yomakase/yomakase/YomakaseApplication.java
+++ src/main/java/com/yomakase/yomakase/YomakaseApplication.java
@@ -2,8 +2,10 @@ package com.yomakase.yomakase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YomakaseApplication {
 
 	public static void main(String[] args) {
