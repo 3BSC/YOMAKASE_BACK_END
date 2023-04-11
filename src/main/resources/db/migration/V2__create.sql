f9852b834242cc52dca960e9757bf9f161721fdd
Insuk <inkcloud365@gmail.com>
2023-04-11 16:47:34 +0900

    feat: fix database schema
    - 주소 스키마 삭제
    - 가게 스키마 영업 시간, 쉬는 시간 추가
    - 가게 스키마 주소 내용 추가

--- src/main/resources/db/migration/V2__create.sql
+++ src/main/resources/db/migration/V2__create.sql
@@ -18,10 +18,19 @@ CREATE TABLE IF NOT EXISTS restaurant
     created_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     is_deleted      TINYINT(1)      NOT NULL DEFAULT 0,
+    name            VARCHAR(20),
     registration    VARCHAR(255),
     category        VARCHAR(10),
     summary         VARCHAR(1000),
-    phone_number    VARCHAR(10)
+    phone_number    VARCHAR(10),
+    service_start   TIME,
+    service_end     TIME,
+    break_start     TIME,
+    break_end       TIME,
+    1depth          VARCHAR(15),
+    2depth          VARCHAR(15),
+    3depth          VARCHAR(15),
+    address_name    VARCHAR(50)     NOT NULL,
 ) default character set utf8mb4
   collate utf8mb4_bin;
 
@@ -42,9 +51,6 @@ CREATE TABLE IF NOT EXISTS price
 CREATE TABLE IF NOT EXISTS closed_days
 (
     id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    is_deleted      TINYINT(1)      NOT NULL DEFAULT 0,
     day             VARCHAR(10)     NOT NULL,
     restaurant_id   BIGINT          NOT NULL,
     FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
@@ -54,19 +60,7 @@ CREATE TABLE IF NOT EXISTS closed_days
 CREATE TABLE IF NOT EXISTS facilities
 (
     id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    facilitie       VARCHAR(10)     NOT NULL,
-    restaurant_id   BIGINT          NOT NULL,
-    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
-) default character set utf8mb4
-  collate utf8mb4_bin;
-
-CREATE TABLE IF NOT EXISTS address
-(
-    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    1depth          VARCHAR(15),
-    2depth          VARCHAR(15),
-    3depth          VARCHAR(15),
-    name            VARCHAR(50)     NOT NULL,
+    facilities      VARCHAR(10)     NOT NULL,
     restaurant_id   BIGINT          NOT NULL,
     FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
 ) default character set utf8mb4
