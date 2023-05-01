1f59f5fd1270d961c4358d9d94ffab17cb31b98b
Insuk <inkcloud365@gmail.com>
2023-05-01 14:37:42 +0900

    feat: add user response
    - UserResponse 추가
    - SignUpResponse 유저 정보 분리

--- src/main/java/com/yomakase/yomakase/user/dto/response/SignUpResponse.java
+++ src/main/java/com/yomakase/yomakase/user/dto/response/SignUpResponse.java
@@ -1,8 +1,6 @@
 package com.yomakase.yomakase.user.dto.response;
 
 import com.yomakase.yomakase.user.dto.Token;
-import com.yomakase.yomakase.user.enums.UserType;
-import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,15 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpResponse {
 
-    private String email;
-
-    private String nickname;
-
-    private Date birthDay;
-
-    private UserType type;
-
-    private String phoneNumber;
+    private UserResponse user;
 
     private Token token;
 }
--- src/main/java/com/yomakase/yomakase/user/dto/response/UserResponse.java
+++ src/main/java/com/yomakase/yomakase/user/dto/response/UserResponse.java
@@ -1,5 +1,20 @@
 package com.yomakase.yomakase.user.dto.response;
 
+import com.yomakase.yomakase.user.enums.UserType;
+import java.util.Date;
+
 public class UserResponse {
+
+    private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    private Date birthDay;
+
+    private UserType type;
+
+    private String phoneNumber;
 
 }
