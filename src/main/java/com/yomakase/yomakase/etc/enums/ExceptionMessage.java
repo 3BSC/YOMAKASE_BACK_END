2c18687a22d164b07ac7e135e4ef2d5fbe10879a
insukL <inkcloud365@gmail.com>
2023-05-28 16:54:15 +0900

    feat: add exception message
    - 예외 메시지 클래스 추가

--- src/main/java/com/yomakase/yomakase/etc/enums/ExceptionMessage.java
+++ src/main/java/com/yomakase/yomakase/etc/enums/ExceptionMessage.java
@@ -1,15 +1,35 @@
 package com.yomakase.yomakase.etc.enums;
 
+import org.springframework.http.HttpStatus;
+
 public enum ExceptionMessage {
+    UNDEFINED_EXCEPTION(0, "정의되지 않은 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    NULL_POINTER_EXCEPTION(1, "NULL 여부를 확인해주세요.", HttpStatus.BAD_REQUEST),
+
+    // User 관련 예외
+    USER_NOT_EXISTS_EXCEPTION(1000, "해당 유저를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+    USER_ALREADY_EXIST(1001, "이미 존재하는 유저입니다.", HttpStatus.CONFLICT),
+
+
+    // 인증, 인가 관련 예외
+    INVALID_TOKEN(12, "유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_ACCESS(13, "유효하지 않은 접근입니다.", HttpStatus.CONFLICT),
+    EXPIRED_TOKEN(14, "만료된 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_TOKEN_TYPE(15,  "잘못된 토큰 타입입니다.", HttpStatus.BAD_REQUEST),
+    
+    
+
     ;
 
 
     private String message;
     private Integer code;
+    private HttpStatus status;
 
-    ExceptionMessage(String message, Integer code) {
+    ExceptionMessage(Integer code, String message, HttpStatus status) {
         this.message = message;
         this.code = code;
+        this.status = status;
     }
 
     public String getMessage() {
@@ -18,5 +38,9 @@ public enum ExceptionMessage {
 
     public Integer getCode() {
         return code;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
